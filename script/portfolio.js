d5f964b8b6e415f93b5b830c41e129f98d14ec62
rizkiismail <laksmimentari@gmail.com>
2023-05-15 21:15:12 +0700

    first

--- script/portfolio.js
+++ script/portfolio.js
@@ -30,22 +30,28 @@ const illustrations = [
 ];
 const cerpen = [
   {
-    title: "Sialan Kau, Bastet. Aku ini menyayangimu",
+    id: 1,
+    title: "Sialan Kau, Bastet, Aku ini menyayangimu",
     capaian: "Rank 19th National, Tulis.Me 2023",
     tanggal: "February 13th 2023",
     src: "https://images.unsplash.com/photo-1662994314076-68a7573a1e23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
+    href: "https://docs.google.com/document/d/1W-XAgLTkSOGF3hbI-_iy_on43l0sqkzPwRawzCaLQJY/edit?usp=sharing",
   },
   {
+    id: 2,
     title: "Matilah Sesuai Keinginanmu",
     capaian: "Rank 13th National, ikutlomba 2020",
     tanggal: "November 30th 2020",
     src: "https://images.unsplash.com/photo-1626663082675-6be9ee82c4ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y29mZmlufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    href: "https://docs.google.com/document/d/1xyGZrB9ooX9NmzPfgD-fdaMRmPRJIgFXIQmgn2QpelU/edit?usp=sharing",
   },
   {
+    id: 3,
     title: "Kisah Manusia Gajah",
     capaian: "Rank 52th National, Tulis.Me 2020",
     tanggal: "September 10th 2020",
     src: "https://images.unsplash.com/photo-1557050543-4d5f4e07ef46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlcGhhbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    href: "https://docs.google.com/document/d/15u_gLOZlD8OkDUvLokvjJgD543kwXdXpPHFswJO2Alo/edit?usp=sharing",
   },
 ];
 
@@ -66,7 +72,7 @@ for (let i = 0; i < cerpen.length; i++) {
   card2.innerHTML += `<div class="card" style="width: 18rem">
   <img src="${cerpen[i].src}" class="card-img-top" alt="..." />
   <div class="card-body">
-    <h5 class="card-title"><a href="#">${cerpen[i].title} </a></h5>
+    <h5 class="card-title"><a href="${cerpen[i].href}">${cerpen[i].title} </a></h5>
     <p style="color: blue"> ${cerpen[i].capaian} </p>
     <p class="card-text"><small class="text-body-secondary"> ${cerpen[i].tanggal} </small></p>
   </div>
